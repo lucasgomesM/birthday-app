8b901e5bc22ea70944d81ffd2cbba770a7a620cd
ireallyhatejava <lucasgomes.martines@gmail..com>
2022-02-16 08:34:48 -0300

    Birthday app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {bdays} from './data';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <List/>
     </div>
   );
+}
+
+function List() {
+  const [people, setPeople] = React.useState(bdays);
+  const clearItems = () => {
+      setPeople([]);
+  }
+  return <section className='list'>  
+      <h1> {people.length} Birthdays!</h1>  
+
+      {people.map((person) => {
+        const {id,name,age,img,msg} = person;
+        return <>
+        
+        <div key={id} className='item'>
+          <img src={img} alt={name} className='image'/>
+          <h3 className='itemAtr'> {name}, {age} years</h3>   
+           <h4 className='itemMsg'>{msg}</h4>         
+          
+          </div>          
+          
+           
+         
+          
+        </>
+    })}
+    <button className='btn' onClick={() => clearItems()}>Clear People</button>
+  </section>
 }
 
 export default App;
